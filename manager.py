42611c0007e56d42977efbb0ff49a198f9c268be
lepoul <lepou@laptop6.ntua.gr>
2017-05-02 17:07:41 +0300

    same

--- manager.py
+++ manager.py
@@ -16,14 +16,14 @@ class Manager:
         self.password = password
     # Connect to a device given its IP and user/pass credentials
     def connect(self):
-        s = cisco_client.transport.Session(ip=self.ip, username=self.username, password=self.password)
+        s = cisco_ncclient.transport.Session(ip=self.ip, username=self.username, password=self.password)
         s.set_channel()
         s.hello_netconf()
         self.__session = s
     # Send an Edit config RPC
     def edit_config(self, filename):
         # command in <cmd>mpla mpla</cmd> format
-        rpc = cisco_client.operations.EditConfigRPC()
+        rpc = cisco_ncclient.operations.EditConfigRPC()
         rpc.build_rpc(filename)
         self.__session.send_rpc(rpc.text)
         logger.info("Configuration pushed")
@@ -36,7 +36,7 @@ class Manager:
     # Get running config
     # Using plain SSH, not NETCONF
     def get_config(self):
-        temp_chan = cisco_client.transport.Session(ip=self.ip, username=self.username, password=self.password)
+        temp_chan = cisco_ncclient.transport.Session(ip=self.ip, username=self.username, password=self.password)
         temp_chan.set_channel()
         config = temp_chan.command("sh run | begin version")
         temp_chan.close()
